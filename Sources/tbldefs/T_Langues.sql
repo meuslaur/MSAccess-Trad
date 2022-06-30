0f4b30cf79edc567a0b810e43a3c26eff7144f7c
Laurent Meus <meuslau@protonmail.com>
2022-06-30 19:28:53 +0200

    Delete Sources directory

--- Sources/tbldefs/T_Langues.sql
+++
@@ -1,4 +0,0 @@
-﻿CREATE TABLE [T_Langues] (
-  [LangueCode] LONG  CONSTRAINT [PrimaryKey] PRIMARY KEY UNIQUE NOT NULL,
-  [LangueNom] VARCHAR (50)
-)
--- Sources/tbldefs/T_TradTexte.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [T_TradTexte] (
-  [Trad_ID] VARCHAR (100),
-  [PropNom] VARCHAR (30),
-  [PropTexte] LONGTEXT ,
-  [LangueCode] LONG  CONSTRAINT [T_LanguesT_TradTexte] REFERENCES [T_Langues] ([LangueCode]) ON UPDATE CASCADE  ON DELETE CASCADE ,
-  [DateMaJ] DATETIME ,
-  [DefLangue] BIT ,
-   CONSTRAINT [PrimaryKey] PRIMARY KEY ([Trad_ID], [PropNom])
-)
